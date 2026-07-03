2739b22cfe88538c36562f4a679fe94e5e449b00
HesquivelV <Hev_1984@hotmail.com>
2026-07-02 20:57:33 -0600

    third commit

---
+++ MODULO_1/clase2/OtraClase.java
@@ -0,0 +1,8 @@
+public class OtraClase {
+    public static void main(String[] args){
+        int entero = 5;
+        double decimal = entero;
+        System.out.println("Valor entero "+ entero);
+        System.out.println("Valor decimal "+ decimal);
+    }
+}
