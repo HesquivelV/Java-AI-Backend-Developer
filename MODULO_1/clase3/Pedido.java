9ae046edb349591ba1223216b3d83366866a5d70
HesquivelV <Hev_1984@hotmail.com>
2026-07-04 18:29:17 -0600

    clase 3 lista

---
+++ MODULO_1/clase3/Pedido.java
@@ -0,0 +1,9 @@
+public class Pedido {
+
+    double calcularTotal(double subtotal, double porcentaje) {
+        double propina = subtotal * porcentaje;
+        return subtotal + propina;
+    }
+}
+
+
